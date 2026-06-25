e76d85ac1ff1dc6abf086c0aeb660ce16643b48a
Khanh-Du <phamkhanhdu1503@gmail.com>
2026-06-25 19:57:30 +0700

    update order detail

--- admin/order_detail.php
+++ admin/order_detail.php
@@ -66,7 +66,7 @@ $items = $stmt->get_result();
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-     <link rel="stylesheet" href="../style/order_detail.css">
+    <link rel="stylesheet" href="../style/order_detail.css">
 
 </head>
 
